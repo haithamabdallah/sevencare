d178705ddc621d29ebfac312cd15e898e2a6e17a
Haitham AbdAllah <haithamabdallah@gmail.com>
2024-08-08 00:36:26 +0300

    sections

--- src/components/sections/Sections.jsx
+++ src/components/sections/Sections.jsx
@@ -1,22 +1,30 @@
 // Sections.jsx
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { sectionsData } from './sectionsData';
+import './sections.css';
 
-const Section = ({ title, items }) => (
-    <div className="section">
-        <h1>{title}</h1>
-        <div className="items">
-            {items.map(item => (
-                <a key={item.id} href={item.href}>
-                    <img src={item.image} alt={`Offer ${item.id}`} />
-                </a>
-            ))}
+const Section = ({ slug, items }) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className="section-section">
+            <h1>{t(`sections.${slug}.title`)}</h1>
+            <ul className="items">
+                {items.map(item => (
+                    <li key={item.id}>
+                        <a href={item.href}>
+                            <img src={item.image} alt={`Offer ${item.id}`} />
+                        </a>
+                    </li>
+                ))}
+            </ul>
         </div>
-    </div>
-);
+    );
+};
 
 const Banner = ({ image, href }) => (
-    <div className="banner">
+    <div className="section-banner">
         <a href={href}>
             <img src={image} alt="Banner" />
         </a>
@@ -25,12 +33,12 @@ const Banner = ({ image, href }) => (
 
 const Sections = () => {
     return (
-        <div className="sections-container">
+        <div className="max-w-[1230px] mx-auto px-[20px]">
             {sectionsData.map(data => (
                 data.type === 'section' ? (
-                    <Section key={data.id} title={data.title} items={data.items} />
+                    <Section key={data.id} slug={data.slug} items={data.items} />
                 ) : (
-                    <Banner key={data.id} image={data.image} href={data.href} />
+                    data.showBanner && <Banner key={data.id} image={data.image} href={data.href} />
                 )
             ))}
         </div>
